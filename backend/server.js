920a12ccd8d0db95d120db78406959d72b9f9eb9
Dhiraj <royd3366@gmail.com>
2025-12-25 19:07:37 +0530

    fixing backend bugs

--- backend/server.js
+++ backend/server.js
@@ -3,34 +3,34 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudnary.js'
+
 import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productRoutes.js'
 import cartRouter from './routes/cartRoutes.js'
 import orderRouter from './routes/orderRoutes.js'
 import reviewRouter from './routes/reviewRoutes.js'
 
-
-//App config
-
+// App config
 const app = express()
-const port = process.env.PORT || 4000
+
+
 connectDB()
 connectCloudinary()
 
-//middlewares
+// Middlewares
 app.use(express.json())
 app.use(cors())
 
-
-//api endpoints
-app.use('/api/user',userRouter)
+// Routes
+app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
-app.use('/api/review',reviewRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
+app.use('/api/review', reviewRouter)
 
-app.get('/',(req,res)=>{
-    res.send("API Working")
+app.get('/', (req, res) => {
+  res.send('API Working 🚀')
 })
 
-app.listen(port,()=>console.log(`Server started on PORT : http://localhost:${port}`))
+
+export default app
